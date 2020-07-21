9a3bf37af1dadac9f409572cf459028cd1e5f967
yasir mohamud <ymohamed018@gmail.com>
2020-07-21 14:30:26 -0700

    adds all files to the db

---
+++ async-inn/async-inn/Data/AsyncInnDbContext.cs
@@ -0,0 +1,21 @@
+﻿using async_inn.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace async_inn.Data
+{
+    public class AsyncInnDbContext : DbContext
+    {
+        public AsyncInnDbContext(DbContextOptions<AsyncInnDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Amenity> Amenities { get; set; }
+    }
+}
---
+++ async-inn/async-inn/Models/Hotel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace async_inn.Models
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Phone { get; set; }
+    }
+}
