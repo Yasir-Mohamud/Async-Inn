78b33dbb52f652c0b598b66de013df4a77651ba4
yasir mohamud <ymohamed018@gmail.com>
2020-07-28 23:01:26 -0700

    adds  some of the hotelroom dto

--- async-inn/async-inn/Models/Services/HotelRoomRepository.cs
+++ async-inn/async-inn/Models/Services/HotelRoomRepository.cs
@@ -1,4 +1,5 @@
 ﻿using async_inn.Data;
+using async_inn.Models.DTOs;
 using async_inn.Models.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -11,9 +12,12 @@ namespace async_inn.Models.Services
     public class HotelRoomRepository : IHotelRoom
     {
         private AsyncInnDbContext _context;
-        public HotelRoomRepository(AsyncInnDbContext context)
+        private IRoom _room;
+
+        public HotelRoomRepository(AsyncInnDbContext context, IRoom room)
         {
             _context = context;
+            _room = room;
         }
 
 
@@ -23,7 +27,7 @@ namespace async_inn.Models.Services
         /// <param name="hotelRoom"> hotelroom object</param>
         /// <param name="hotelId">hotel identifier</param>
         /// <returns> Task completion </returns>
-        public async Task<HotelRoom> Create(HotelRoom hotelRoom, int hotelId)
+        public async Task<HotelRoomDTO> Create(HotelRoomDTO hotelRoom, int hotelId)
         {
             hotelRoom.HotelId = hotelId;
             // adds to the database
@@ -38,12 +42,17 @@ namespace async_inn.Models.Services
         /// </summary>
         /// <param name="hotelId"> hotel identifier</param>
         /// <returns> list of hotel rooms</returns>
-        public async Task<List<HotelRoom>> GetHotelRooms(int hotelId)
+        public async Task<List<HotelRoomDTO>> GetHotelRooms(int hotelId)
         {
             List<HotelRoom> hotelRoom = await _context.HotelRooms.Where(x => x.HotelId == hotelId)
                                                                  .Include(x => x.room)
-                                                                 .ToListAsync();
-            return hotelRoom;
+                                                                   .ToListAsync();
+            var  dto = new List<HotelRoomDTO>();
+            foreach (var item in hotelRoom)
+            {
+                dto.Add(await GetHotelRoom(item.HotelId, item.RoomNumber));
+            }
+            return dto;
         }
 
 
@@ -53,16 +62,28 @@ namespace async_inn.Models.Services
         /// <param name="roomNumber">identifies the number of the room </param>
         /// <param name="hotelId">hotel identifier</param>
         /// <returns>Hotel room with room and amenities</returns>
-        public async Task<HotelRoom> GetHotelRoom(int hotelId, int roomNumber)
+        public async Task<HotelRoomDTO> GetHotelRoom(int hotelId, int roomNumber)
         {
-            var hotelRoom = await _context.HotelRooms.Where(x => x.HotelId == hotelId && x.RoomNumber == roomNumber)
+            HotelRoom hotelRoom = await _context.HotelRooms.Where(x => x.HotelId == hotelId && x.RoomNumber == roomNumber)
                                                      .Include(x => x.hotel)
                                                      .Include(x => x.room)
                                                      .ThenInclude(x => x.RoomAmenities )
                                                      .ThenInclude(x => x.amenity)
                                                      .FirstOrDefaultAsync();
 
-            return hotelRoom;
+            RoomDTO roomdto = await _room.GetRoom(hotelRoom.RoomId);
+            HotelRoomDTO dto = new HotelRoomDTO()
+            {
+                HotelId = hotelRoom.HotelId,
+                RoomNumber = hotelRoom.RoomNumber,
+                Rate = hotelRoom.Rate,
+                PetFriendly = hotelRoom.PetFriendly,
+                RoomId = hotelRoom.RoomId,
+                Room = roomdto
+               
+            };
+       
+            return dto;
         }
 
         /// <summary>
@@ -70,7 +91,7 @@ namespace async_inn.Models.Services
         /// </summary>
         /// <param name="hotelRoom"> the hotel room object </param>
         /// <returns> task completion </returns>
-        public async Task Update(HotelRoom hotelRoom)
+        public async Task Update(HotelRoomDTO hotelRoom)
         {
             _context.Entry(hotelRoom).State = EntityState.Modified;
             await _context.SaveChangesAsync();
@@ -84,7 +105,7 @@ namespace async_inn.Models.Services
         /// <returns> task completion </returns>
         public async Task Delete(int hotelId, int roomNumber)
         {
-            HotelRoom hotelRoom = await GetHotelRoom(hotelId, roomNumber);
+            HotelRoom hotelRoom = await _context.HotelRooms.FindAsync(hotelId, roomNumber);
             _context.Entry(hotelRoom).State = Microsoft.EntityFrameworkCore.EntityState.Deleted;
             await _context.SaveChangesAsync();
         }
